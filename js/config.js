512a25f4a0eaf69c183bf86aed9ea2dfcfa9cff7
Hayden <hayden.keers@siemens.com>
2020-07-30 11:09:40 +1000

    Updated cognito client app

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-2_5JT0fbf5x', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3q0n123cctka7k8sdkok2c75e3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'tqh14lmhmdehjcm6vspc9i47d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
